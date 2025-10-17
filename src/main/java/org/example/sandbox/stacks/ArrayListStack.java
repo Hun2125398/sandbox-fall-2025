73ae5f0f85a1bb724588345f103776fc48f47295
Spyro1001 <raymondhedgecock@gmail.com>
2025-10-16 19:00:17 -0700

    adding stack code

--- src/main/java/org/example/sandbox/stacks/ArrayListStack.java
+++ src/main/java/org/example/sandbox/stacks/ArrayListStack.java
@@ -2,8 +2,9 @@ package org.example.sandbox.stacks;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class ArrayListStack<E> implements Stack<E> {
+public class ArrayListStack<E extends Comparable<E>> implements Stack<E> {
 
     private List<E> stack;
 
@@ -13,26 +14,45 @@ public class ArrayListStack<E> implements Stack<E> {
 
     @Override
     public boolean empty() {
-        return false;
+        return this.stack.isEmpty();
     }
 
     @Override
     public E peek() {
-        return null;
+        return this.stack.get(0);
     }
 
     @Override
     public E pop() {
-        return null;
+        return this.stack.remove(0);
     }
 
     @Override
     public void push(E item) {
-
+        this.stack.add(0, item);
     }
 
     @Override
     public int search(E element) {
-        return 0;
+        return this.stack.indexOf(element) + 1;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(stack);
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ArrayListStack<?> that = (ArrayListStack<?>) o;
+        return Objects.equals(stack, that.stack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(stack);
     }
 }
